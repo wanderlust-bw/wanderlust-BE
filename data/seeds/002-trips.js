cdebf8209719bfb5522e3c599c737d52de0473c8
Yeshi <tyeshi777@yahoo.com>
2019-05-21 19:15:50 -0700

    changed to del

--- data/seeds/002-trips.js
+++ data/seeds/002-trips.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex("trips")
-    .truncate()
+    .del()
     .then(function() {
       // Inserts seed entries
       return knex("trips").insert([
