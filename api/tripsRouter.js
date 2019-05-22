516cf98c2b108db8ea7a5370ed65cef61d48fda3
Yeshi <tyeshi777@yahoo.com>
2019-05-22 12:29:47 -0700

    getById

--- api/tripsRouter.js
+++ api/tripsRouter.js
@@ -3,6 +3,10 @@ const Trips = require("../helpers/universalModel")("trips");
 
 const { authenticate } = require("../auth/authenticate");
 
+function errorHandler(err, res) {
+  res.status(500).json({ msg: `error retrieving the data`, err });
+}
+
 router.get("/trip", authenticate, (req, res) => {
   Trips.get("trips")
     .then(trips => {
@@ -12,13 +16,22 @@ router.get("/trip", authenticate, (req, res) => {
       res.status(500).json(err);
     });
 });
+router.get("/trip/:id", authenticate, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const experiences = await Trips.getById(id);
+    return res.status(200).json(experiences);
+  } catch (err) {
+    return errorHandler(err, res);
+  }
+});
 router.post("/trip", authenticate, async (req, res) => {
   try {
     const experiences = req.body;
     const newExperiences = await Trips.add(experiences);
     return res.status(201).json(newExperiences);
   } catch (err) {
-    console.log(err);
+    return errorHandler(err, res);
   }
 });
 router.put("/trip/:id", authenticate, async (req, res) => {
@@ -28,7 +41,7 @@ router.put("/trip/:id", authenticate, async (req, res) => {
     const experience = await Trips.edit(id, changes);
     return res.status(200).json({ msg: "update success", experience });
   } catch (err) {
-    console.log(err);
+    return errorHandler(err, res);
   }
 });
 router.delete("/trip/:id", authenticate, async (req, res) => {
@@ -37,7 +50,7 @@ router.delete("/trip/:id", authenticate, async (req, res) => {
     const experience = await Trips.remove(id);
     return res.status(200).json({ msg: "delete success", experience });
   } catch (err) {
-    console.log(err);
+    return errorHandler(err, res);
   }
 });
 module.exports = router;
