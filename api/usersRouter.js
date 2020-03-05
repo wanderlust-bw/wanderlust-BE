d70b06dc9f0c7c7468f9da1557f3729dc9a57c0c
Yeshi <tyeshi777@yahoo.com>
2020-03-05 15:40:56 -0800

    minor change

--- api/usersRouter.js
+++ api/usersRouter.js
@@ -3,13 +3,14 @@ const bcrypt = require("bcryptjs");
 const Users = require("../helpers/universalModel")("users");
 const jwt = require("jsonwebtoken");
 const secrets = require("../config/secrets.js");
+const Joi = require("@hapi/joi");
 
 const { authenticate } = require("../auth/authenticate");
 
 function errorHandler(err, res) {
   res.status(500).json({ msg: `error retrieving the data`, err });
 }
-
+// this is a register post
 router.post("/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 14);
@@ -26,25 +27,28 @@ router.post("/register", (req, res) => {
 
 router.post("/login", (req, res) => {
   let { username, password } = req.body;
+  if (checkInput(username, password).value) {
+    Users.getBy({ username })
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user);
 
-  Users.getBy({ username })
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user);
-
-        res.status(200).json({
-          userId: user.id,
-          userType: user.userType,
-          token: token
-        });
-      } else {
-        res.status(401).json({ message: "invalid credentials" });
-      }
-    })
-    .catch(err => {
-      // console.log(err);
-      res.status(500).json(err.message);
-    });
+          res.status(200).json({
+            userId: user.id,
+            userType: user.userType,
+            token: token
+          });
+        } else {
+          res.status(401).json({ message: "invalid credentials" });
+        }
+      })
+      .catch(err => {
+        // console.log(err);
+        res.status(500).json(err.message);
+      });
+  } else {
+    res.status(400).json(checkInput(username, password).error);
+  }
 });
 
 router.get("/:id", authenticate, async (req, res) => {
@@ -68,5 +72,18 @@ function generateToken(user) {
     expiresIn: "24hr"
   };
   return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
+function checkInput(username, password) {
+  const schema = Joi.object({
+    username: Joi.string()
+      .alphanum()
+      .min(3)
+      .max(30)
+      .required(),
+
+    password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/)
+  });
+  return schema.validate({ username, password });
 }
 module.exports = router;
